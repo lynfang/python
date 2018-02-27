7a8ebfbc91f58a3c62f97a947e6ac787e03ae85c
lynfang <fanglinlyn@gmail.com>
2018-02-27 19:47:54 +0800

    pactise 20180227

---
+++ test123123123.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+import time
+import string
+import os
+import re
+
+a = 12345
+b = 0
+for int_a in str(a):
+    b = int(int_a) + b
+
+print b    
--- tongwen.py
+++ tongwen.py
@@ -3,3 +3,19 @@ import time
 import string
 import os
 import re
+
+#tongwen_list = []
+sp_tongwen_list = []
+for  tongwen in range(100,999):
+    a = str(tongwen)[::-1]
+    if a == str(tongwen):
+        template1 = 0
+        #print a
+        #tongwen_list.append(a)
+        for a_num in str(a):
+            template1 = template1 + int(a_num)
+        count[str(a)] = template1
+            
+for test in count.keys():
+    if count[test] < 15:
+        print test
